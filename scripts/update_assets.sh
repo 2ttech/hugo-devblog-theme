3da4841c5300c512beda85c032fe76f5373ffcfe
Terje Torkelsen <tertork@gmail.com>
2020-05-13 21:28:56 +0200

    Added package.json file and installing bootstrap

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -2,13 +2,9 @@
 # Script to update assets 
 # Use: run from project root dir.
 
-ASSETS_JS_DIR="assets/js/vendor/"
-ASSETS_SCSS_DIR="assets/scss/vendor/"
+ASSETS_SCSS_DIR="assets/scss/"
 
 mkdir -p $ASSETS_SCSS_DIR/bootstrap/
 
 #cp node_modules/bootstrap/dist/js/bootstrap.min.js $ASSETS_JS_DIR
 cp -r node_modules/bootstrap/scss/* $ASSETS_SCSS_DIR/bootstrap/
-
-# cp node_modules/jquery/dist/jquery.min.js $ASSETS_JS_DIR
-# cp node_modules/popper.js/dist/umd/popper.min.js $ASSETS_JS_DIR
