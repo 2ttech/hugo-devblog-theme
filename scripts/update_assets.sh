6795fa0635390c93e6575ecb4dd517d07609af8c
Terje Torkelsen <tertork@gmail.com>
2020-05-13 21:38:03 +0200

    Updated bootstrap package

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -6,5 +6,8 @@ ASSETS_SCSS_DIR="assets/scss/"
 
 mkdir -p $ASSETS_SCSS_DIR/bootstrap/
 
+# Remove folder first and then resync so deleted files will go away
+rm -fr $ASSETS_SCSS_DIR/bootstrap/scss
+
 #cp node_modules/bootstrap/dist/js/bootstrap.min.js $ASSETS_JS_DIR
-cp -r node_modules/bootstrap/scss/* $ASSETS_SCSS_DIR/bootstrap/
+cp -r node_modules/bootstrap/scss $ASSETS_SCSS_DIR/bootstrap/
